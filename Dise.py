9fab58a3c19eb9859ff904791a924d2483afa778
ho-ryu-ji <ryuta.soc7@gmail.com>
2023-06-15 01:13:11 +0900

    Dice game: initial revision

---
+++ Dise.py
@@ -0,0 +1,13 @@
+import random
+
+print('Rolling dice...')
+
+nums = [1, 2, 3, 4, 5, 6]
+
+die1 = random.choice(nums)
+print('Die 1: %.0f'%die1)
+die2 = random.choice(nums)
+print('Die 2: %.0f'%die2)
+
+sum = die1+die2
+print('Total value: %.0f'%sum)
